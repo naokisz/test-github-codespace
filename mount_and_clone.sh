e241ffd7d549c80976ba8ff88fc7d2751760593f
naoki suzuki <naokihirokitomoki@gmail.com>
2024-08-27 15:55:33 +0000

    fix: Add cache dir

--- mount_and_clone.sh
+++ mount_and_clone.sh
@@ -4,3 +4,4 @@ cd /workspaces/docker-services/rclone-mount
 docker compose up -d
 cd /workspaces
 GIT_SSH_COMMAND="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" git clone ssh://vscode@172.17.0.1:2222/workspaces/onedrive/repo/tex.git --branch gitdoc --depth 1
+mkdir -p /home/vscode/.texlive2024/texmf-var/luatex-cache
