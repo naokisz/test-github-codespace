3ec964f62397a7f0eae89edda1b7692636ef5fab
naoki suzuki <naokihirokitomoki@gmail.com>
2024-08-27 15:28:36 +0000

    feat: Add files

--- README.md
+++ README.md
@@ -2,15 +2,14 @@
 
 ```bash
 ./setup.sh
-sudo passwd vscode
 ```
 
+port forward 2222
+vscode Remote-SSH
 copy rclone.conf to /workspaces/rclone_config/.rclone.conf
 
 ```bash
 ./mount_and_clone.sh
 ```
 
-port forward 2222
-vscode Remote-SSH
 Reopen devcontainer
--- setup.sh
+++ setup.sh
@@ -4,10 +4,15 @@ ssh-keygen -t ed25519
 cat ~/.ssh/id_ed25519.pub >> ~/.ssh/authorized_keys
 # ssh -p 2222 -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o GlobalKnownHostsFile=/dev/null vscode@localhost
 
-sudo apt install keychain
+export DEBIAN_FRONTEND=noninteractive
+sudo -E apt-get update
+sudo -E apt-get install -y -qq keychain
 cat ./keychain_config.sh >> ~/.bashrc
 
 cd /workspaces
 git clone https://github.com/Mumie-hub/docker-services.git
 cp /workspaces/test-github-codespace/docker-compose.yml /workspaces/docker-services/rclone-mount
 mkdir /workspaces/rclone_config
+sudo mount --make-shared /
+sudo mount --make-shared /workspaces
+sudo passwd vscode
