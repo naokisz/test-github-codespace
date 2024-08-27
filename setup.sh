5646ccc7aa8eb4140de822aa14f0ec750e06332a
naoki suzuki <naokihirokitomoki@gmail.com>
2024-08-27 15:32:22 +0000

    fix: Nonintractive ssh-keygen

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ssh-keygen -t ed25519
+ssh-keygen -t ed25519 -q -f "$HOME/.ssh/id_ed25519" -N ""
 cat ~/.ssh/id_ed25519.pub >> ~/.ssh/authorized_keys
 # ssh -p 2222 -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o GlobalKnownHostsFile=/dev/null vscode@localhost
 
